e88caedb2cd45c7acca4cced80bafba6666069f0
Rubin Daija <daija_r@hotmail.com>
2017-10-19 21:52:39 +0300

    Most of the gameplay
    ----There are no comments in this file they will be added later----
    All the pieces are present.
    All their movements are done.
    There is no collision checking, eventhough pieces can not take any piece right now.
    There are no rules except of their way of moving.
    Compile and test the code for more.

---
+++ Test Codes/boardTest1.2/src/Bishop.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+
+public class Bishop extends Piece{
+        public Bishop(int initialX, int initialY, String fullPicName, String halfPicName, int sizex, int sizey, Color color, int player){
+            super(initialX,initialY,fullPicName,halfPicName,sizex,sizey,color,player);
+            calculatePossibleMoves();
+        }
+
+        @Override
+        public void move(int x, int y) {
+
+        }
+
+        protected void calculatePossibleMoves(){
+            super.calculatePossibleMoves();
+
+            int tmp;
+
+            for (int i = 0; i <= 7; i++){
+                tmp = posY - i;
+                if ( tmp >= 0) {
+                    positions.add(new Point(posX + i, tmp));
+                } else{
+                    break;
+                }
+            }
+            positions.add(null);
+
+            for (int i = 0; i <= 7; i++){
+                tmp = posX + i;
+                if (tmp < 8) {
+                    positions.add(new Point(tmp, posY + i));
+                } else{
+                    break;
+                }
+            }
+            positions.add(null);
+
+            for (int i = 0; i <= 7; i++){
+                tmp = posY + i;
+                if (tmp < 8) {
+                    positions.add(new Point(posX - i , tmp));
+                } else{
+                    break;
+                }
+            }
+            positions.add(null);
+
+            for (int i = 0; i <= 7; i++){
+                tmp = posX - i;
+                if (tmp >= 0) {
+                    positions.add(new Point(tmp, posY - i));
+                } else{
+                    break;
+                }
+            }
+
+
+
+
+        }
+}
