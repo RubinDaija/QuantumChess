89285412becd8da616db9dd0323f7a7f85c361c7
Serhat Bezmez <31791274+jrkb6@users.noreply.github.com>
2017-11-04 14:27:10 +0300

    Packaged project and final report my part

---
+++ PackagedSources/QuantumChessGame/src/EntityModel/Pawn.java
@@ -0,0 +1,28 @@
+package EntityModel;
+
+import java.awt.*;
+
+public class Pawn extends Piece {
+
+    public Pawn(int initialX, int initialY, String fullPicName, String halfPicName, int sizex, int sizey, Color color, int player){
+        super(initialX,initialY,fullPicName,halfPicName,sizex,sizey,color,player);
+        calculatePossibleMoves();
+    }
+
+
+    @Override
+    public void move(int x, int y) {
+
+    }
+
+
+    protected void calculatePossibleMoves() {
+        super.calculatePossibleMoves();
+        if (player == 1) {
+            positions.add(new Point(posX, posY -1));
+        } else {
+            positions.add(new Point(posX, posY + 1));
+
+        }
+    }
+}
