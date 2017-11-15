5e4ce4c47450e5acb9b9bbd85739e592dbc0de63
akturkkaan <kaan-akturk@windowslive.com>
2017-11-15 09:39:33 +0200

    Playscreen
    Playscreen

--- Playscreen/gameScreen.java
+++ Playscreen/gameScreen.java
@@ -1,27 +1,36 @@
 import java.awt.*; 
 import javax.swing.*;
 
+
 public class gameScreen //extends JFrame
 {
   
   public static void main(String[] args) 
   {
     JFrame frame = new JFrame("JLabel");
+    
     JLabel background=new JLabel(new ImageIcon("background.jpg"));
+    
     int frameSizeX = 1000;
     int frameSizeY = 1000;
+    
     frame.setSize(frameSizeX, frameSizeY);
     
     //Putting the main frame into center of the window 
+    
     Dimension windowSize = frame.getSize();
+    
     GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
     Point centerPoint = ge.getCenterPoint();
+    
     int dx = centerPoint.x - windowSize.width / 2;
     int dy = centerPoint.y - windowSize.height / 2;  
     
     //Tunneling    
+    
     Panel tunnellingPanel=new Panel();
     Button tunnelingButton=new Button("Tunneling");
+    
     tunnellingPanel.setBounds(frameSizeX-200,100, dx,  50 );
     tunnellingPanel.setLayout(new BoxLayout(tunnellingPanel, BoxLayout.Y_AXIS));
     
@@ -29,14 +38,17 @@ public class gameScreen //extends JFrame
     JLabel tunnelingLogo=new JLabel(new ImageIcon("tunneling.jpg"));
     tunnelingLogo.setBounds(frameSizeX-600,0, frameSizeY-100,  100);
     tunnellingPanel.add(tunnelingButton);
+    
     background.add(tunnelingLogo);
     background.add(tunnellingPanel);
 
 
     
     //Entanglement
+    
     Panel engtanglementPanel=new Panel();
     Button engtanglementButton=new Button("Entanglement");
+    
     engtanglementPanel.setBounds(frameSizeX-200,260, dx,  50 );
     engtanglementPanel.setLayout(new BoxLayout(engtanglementPanel, BoxLayout.Y_AXIS));
     
@@ -44,40 +56,50 @@ public class gameScreen //extends JFrame
     JLabel entanglementLogo=new JLabel(new ImageIcon("entanglement.jpg"));
     entanglementLogo.setBounds(frameSizeX-600,0, frameSizeY-100,  410);
     engtanglementPanel.add(engtanglementButton);
+    
     background.add(entanglementLogo);
     background.add(engtanglementPanel);
     
     //Superposition 
+    
     Panel superpositionPanel=new Panel();
     Button superpositionButon=new Button("Superposition");
+    
     superpositionPanel.setBounds(frameSizeX-200,430, dx,  50 );
     superpositionPanel.setLayout(new BoxLayout(superpositionPanel, BoxLayout.Y_AXIS));
+    
     
     superpositionPanel.setSize(120,50);
     JLabel superPositionLogo=new JLabel(new ImageIcon("superposition.jpg"));
     superPositionLogo.setBounds(frameSizeX-600,0, frameSizeY-100,  740);
     superpositionPanel.add(superpositionButon);
+    
     background.add(superPositionLogo);
     background.add(superpositionPanel);
     
     
     //Cancel
+    
     Panel cancelPanel=new Panel();
     Button cancelButton=new Button("Cancel");
+    
     cancelPanel.setBounds(frameSizeX-200,530, dx,  50 );
     cancelPanel.setLayout(new BoxLayout(cancelPanel, BoxLayout.Y_AXIS));  
     cancelPanel.setSize(120,50);
     cancelPanel.add(cancelButton);
+    
     background.add(cancelPanel);
     
     
     //Surrender
     Panel surrenderPanel=new Panel();
     Button surrenderButton=new Button("Surrender");
+    
     surrenderPanel.setBounds(frameSizeX-200,600, dx,  50 );
     surrenderPanel.setLayout(new BoxLayout(surrenderPanel, BoxLayout.Y_AXIS));  
     surrenderPanel.setSize(120,50);
     surrenderPanel.add(surrenderButton);
+    
     background.add(surrenderPanel);
     
     
