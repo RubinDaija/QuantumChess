53a8d93c03be3b4f1ade147253cb12db42beec79
Serhat Bezmez <31791274+jrkb6@users.noreply.github.com>
2017-12-16 20:50:03 +0300

    WinnerPopUp button actions

--- Code/Code with one thread only/QuantumChessGame/src/Views/WinnerPopUp.java
+++ Code/Code with one thread only/QuantumChessGame/src/Views/WinnerPopUp.java
@@ -1,9 +1,13 @@
 package Views;
 
+import EntityModel.GameManager;
 import EntityModel.Player;
+import EntityModel.QuantumChess;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class WinnerPopUp extends JFrame{
     ImageIcon bck = new ImageIcon("congratulations.png"); /* to get Image size */
@@ -11,6 +15,8 @@ public class WinnerPopUp extends JFrame{
     JPanel winnerName = new JPanel();
     JLabel playerInfo = new JLabel();
     JButton ok = new JButton("OK");
+    JButton quit = new JButton("Quit");
+    MouseListener mls;
     public  WinnerPopUp(Player player){
 
         this.playerInfo.setText("<html><center>Congratz to player"+player.getPlayerNo() + "<br>"+player.getPlayerName()+" </center></html>"); //use html for multi lines
@@ -18,11 +24,49 @@ public class WinnerPopUp extends JFrame{
         background.add(playerInfo);
         this.setContentPane(background);
         this.setPreferredSize(new Dimension(bck.getIconWidth(),bck.getIconHeight()));
+
         this.setTitle("Congratz to Winner");
         this.pack();
         this.setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
-    public void initComponents(){}
+    void setButtonListeners(){
+        mls = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getSource().equals(ok)){
+                    new MenuManager();
+                }
+                else if(e.getSource().equals(quit)){
+                    dispose();
+                    //GameManager.dispose();
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        };
+        ok.addMouseListener(mls);
+        quit.addMouseListener(mls);
+    }
 
     public static void main(String []args)
     {
